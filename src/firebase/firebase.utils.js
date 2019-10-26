7afd2f1d5109e212c2dfc51bcd2748cbae153a19
nachofelpete <nachofelpete@gmail.com>
2019-10-26 18:59:47 +0200

    utilizados los snapshot y document reference en firebase para almancenar y actualizar el current user (estado en app.js)

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
     appId: "1:287686901631:web:85fbb5ef13c8e3cc0a644a"
   };
 
+  //creamos una función para pasar el objeto userAuth y otros datos (para el sign up)
+  export const createUserProfileDocument = async (userAuth, aditionalData) => {
+    if(!userAuth) return;
+
+    //userRef es un Reference doc que se cre y se devuelve siempre por firebase
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+    console.log(snapShot.exists);
+
+    if(!snapShot.exists) {
+       const {displayName, email} = userAuth;
+       const createdAt = new Date();
+
+       try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...aditionalData
+        })
+       } catch (error) {
+         console.log('hubo un problema al crear el usuario', error.message);
+       }
+    }
+    return userRef;
+  }
+  
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
